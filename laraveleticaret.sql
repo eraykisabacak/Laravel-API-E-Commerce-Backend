ef1c296e5f28aafaafe4c3f3ef4469fe2771af8f
Eray Kısabacak <nefret19981998@hotmail.com>
2020-04-05 21:18:01 +0300

    Docker Dosyaları

--- laraveleticaret.sql
+++ laraveleticaret.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 04 Nis 2020, 15:26:42
+-- Üretim Zamanı: 05 Nis 2020, 12:33:06
 -- Sunucu sürümü: 8.0.17
 -- PHP Sürümü: 7.3.10
 
@@ -96,11 +96,13 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`id`, `name`, `status`, `description`, `photo`, `created_at`, `updated_at`) VALUES
 (1, 'Market', 0, 'Market Ürünleri', 'https://www.marketingturkiye.com.tr/wp-content/uploads/2019/03/market.jpg', NULL, '2020-04-04 11:09:05'),
-(2, 'Telefon', 0, 'Telefon, Akıllı Telefon SmartPhone', 'https://images-na.ssl-images-amazon.com/images/I/712FrYrG05L._AC_SL1500_.jpg', NULL, '2020-04-04 11:11:31'),
+(2, 'Telefon', 1, 'Telefon, Akıllı Telefon SmartPhone', 'https://images-na.ssl-images-amazon.com/images/I/712FrYrG05L._AC_SL1500_.jpg', NULL, '2020-04-05 06:30:08'),
 (3, 'Fotograf Makinesi', 1, 'Fotograf Makinesi', 'https://images-na.ssl-images-amazon.com/images/I/91CdhsQNSXL._AC_SX679_.jpg', '2020-03-29 07:57:47', '2020-04-04 11:13:03'),
 (4, 'Yazılım', 1, 'Yazılım Ürünleri', 'https://img-trendyol.mncdn.com/mnresize/400/-/assets/product/images2/20190919/10/249543/56749254/1/1_org_zoom.jpg', '2020-03-30 13:05:25', '2020-03-30 13:05:25'),
 (5, 'Bilgisayar', 1, 'Bilgisayar', 'Bilgisayar', '2020-03-31 15:15:05', '2020-03-31 15:15:05'),
-(7, 'Moda', 1, 'Moda Giyim', 'https://statics.boyner.com.tr/contents/images/dis-giyim(2).jpg', '2020-04-04 11:41:25', '2020-04-04 11:41:25');
+(7, 'Moda', 1, 'Moda Giyim', 'https://statics.boyner.com.tr/contents/images/dis-giyim(2).jpg', '2020-04-04 11:41:25', '2020-04-04 11:41:25'),
+(8, 'Akıllı Saat', 0, 'Akıllı Saat', 'https://images-na.ssl-images-amazon.com/images/I/61yIuX-0DeL._AC_SL1500_.jpg', '2020-04-05 06:34:45', '2020-04-05 06:34:45'),
+(9, 'Oyun Konsolu', 1, 'Oyun Konsolu', 'https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125088306/125088306_5_MC/8830925996082_1583990019559.jpg', '2020-04-05 06:35:28', '2020-04-05 06:35:28');
 
 -- --------------------------------------------------------
 
@@ -221,14 +223,8 @@ CREATE TABLE `sepets` (
 --
 
 INSERT INTO `sepets` (`id`, `status`, `customer_id`, `total_amount`, `shipping_address_id`, `billing_address_id`, `created_at`, `updated_at`) VALUES
-(1, 1, 5, 1000, 1, 1, '2020-04-02 11:29:27', '2020-04-02 11:29:27'),
-(2, 1, 10, 0, 1, 1, '2020-04-04 08:22:29', '2020-04-04 08:22:29'),
-(3, 1, 10, 0, 1, 1, '2020-04-04 08:30:15', '2020-04-04 08:30:15'),
-(4, 1, 10, 0, 1, 1, '2020-04-04 08:30:52', '2020-04-04 08:30:52'),
-(5, 1, 10, 0, 1, 1, '2020-04-04 10:29:49', '2020-04-04 10:29:49'),
-(6, 1, 10, 0, 1, 1, '2020-04-04 10:30:31', '2020-04-04 10:30:31'),
-(7, 1, 10, 0, 1, 1, '2020-04-04 10:31:09', '2020-04-04 10:31:09'),
-(8, 1, 9, 3150, 2, 2, '2020-04-04 10:34:35', '2020-04-04 10:34:35');
+(8, 1, 9, 3150, 2, 2, '2020-04-04 10:34:35', '2020-04-04 10:34:35'),
+(9, 1, 10, 230, 1, 1, '2020-04-05 08:19:58', '2020-04-05 08:19:58');
 
 -- --------------------------------------------------------
 
@@ -251,16 +247,10 @@ CREATE TABLE `sepet_elemanlaris` (
 --
 
 INSERT INTO `sepet_elemanlaris` (`id`, `basket_id`, `product_id`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
-(1, 0, 3, 1, 1000, '2020-04-04 10:29:11', '2020-04-04 10:29:11'),
-(2, 0, 4, 1, 300, '2020-04-04 10:29:11', '2020-04-04 10:29:11'),
-(3, 0, 3, 1, 1000, '2020-04-04 10:29:49', '2020-04-04 10:29:49'),
-(4, 0, 4, 1, 300, '2020-04-04 10:29:49', '2020-04-04 10:29:49'),
-(5, 0, 11, 1, 300, '2020-04-04 10:30:31', '2020-04-04 10:30:31'),
-(6, 0, 4, 1, 300, '2020-04-04 10:30:31', '2020-04-04 10:30:31'),
-(7, 7, 6, 1, 150, '2020-04-04 10:31:10', '2020-04-04 10:31:10'),
-(8, 7, 4, 1, 300, '2020-04-04 10:31:10', '2020-04-04 10:31:10'),
 (9, 8, 13, 1, 3000, '2020-04-04 10:34:36', '2020-04-04 10:34:36'),
-(10, 8, 6, 1, 150, '2020-04-04 10:34:36', '2020-04-04 10:34:36');
+(10, 8, 6, 1, 150, '2020-04-04 10:34:36', '2020-04-04 10:34:36'),
+(11, 9, 11, 1, 80, '2020-04-05 08:19:59', '2020-04-05 08:19:59'),
+(12, 9, 6, 1, 150, '2020-04-05 08:19:59', '2020-04-05 08:19:59');
 
 -- --------------------------------------------------------
 
@@ -286,18 +276,19 @@ CREATE TABLE `uruns` (
 --
 
 INSERT INTO `uruns` (`id`, `name`, `category_id`, `status`, `price`, `stock`, `description`, `photo`, `created_at`, `updated_at`) VALUES
-(3, 'Sony Xperia XZ', 2, 1, 2500, 10, 'Sony Xperia XZ 32 GB Akıllı Telefon', 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/SONY/thumb/v2-83603_small.jpg', '2020-03-29 05:41:18', '2020-04-04 11:15:33'),
-(4, 'Sennheiser Momentum True Wireless Kulaklık', 5, 1, 1700, 10, 'Sennheiser Momentum True Wireless Kulaklık', 'https://productimages.hepsiburada.net/s/23/500/10060027625522.jpg', '2020-03-29 06:46:07', '2020-04-04 11:35:35'),
+(3, 'Sony Xperia XZ', 2, 0, 2500, 10, 'Sony Xperia XZ 32 GB Akıllı Telefon', 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/SONY/thumb/v2-83603_small.jpg', '2020-03-29 05:41:18', '2020-04-05 08:24:13'),
+(4, 'Sennheiser Momentum True Wireless Kulaklık', 5, 1, 1700, 10, 'Sennheiser Momentum True Wireless Kulaklık', 'https://productimages.hepsiburada.net/s/23/500/10060027625522.jpg', '2020-03-29 06:46:07', '2020-04-05 06:28:40'),
 (5, 'Dezenfektan', 1, 1, 60, 10, 'Dermosept El Dezenfektanı 1000 ML', 'https://cdn.dsmcdn.com/assets/dev/product/media/images/20200323/15/107/66284958/1/1_org_zoom.jpg', '2020-03-29 08:11:58', '2020-04-04 11:22:57'),
 (6, 'Microsoft Office', 4, 1, 150, 10, 'Microsoft Office Yazılımı', 'https://img-trendyol.mncdn.com/mnresize/400/-/assets/product/images2/20190919/10/249543/56749254/1/1_org_zoom.jpg', '2020-03-30 13:07:04', '2020-03-30 13:07:04'),
 (11, 'Siyah Koruyucu Maske (COVID-19)', 1, 1, 80, 10, 'Tarz Siyah maske İmaj Hijyen #EVDEKAL', 'https://n11scdn.akamaized.net/a1/450/otomotiv-motosiklet/maske-ve-balaklava/toz-maskesi-3-adet-koruyucu-tarz-siyah-maske-imaj-hijyen__0829911751373914.jpg', '2020-03-31 15:10:29', '2020-04-04 11:32:51'),
 (12, 'Apple MacBook Pro 16', 5, 1, 15000, 2, 'Apple Macbook Pro 16 inch', 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTEwyWk9l9saRLZRiR0tNyUjgHeAHfc5PhVkwOAFk8bHTFbPXSLbg&usqp=CAc', '2020-03-31 15:11:27', '2020-04-04 11:31:16'),
-(13, 'Dell Notebook', 5, 1, 3000, 10, 'Dell Notebook Insprion', 'https://cdn.akakce.com/dell/dell-inspiron-7567-4b70d128f161c-i7-7700hq-16-gb-1-tb-gtx-1050-ti-15-6-z.jpg', '2020-03-31 15:12:20', '2020-04-04 11:14:42'),
+(13, 'Dell Notebook', 5, 0, 3000, 10, 'Dell Notebook Insprion', 'https://cdn.akakce.com/dell/dell-inspiron-7567-4b70d128f161c-i7-7700hq-16-gb-1-tb-gtx-1050-ti-15-6-z.jpg', '2020-03-31 15:12:20', '2020-04-05 06:28:48'),
 (15, 'iPhone 11 128 GB', 2, 1, 7200, 3, 'iPhone 11 128 GB', 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTlv5bVd6MToZtGu7gMhqHP38QkJU9AycXs74C3MOTtWgGZfGxonQ&usqp=CAc', '2020-03-31 15:30:20', '2020-04-04 12:04:41'),
 (16, 'Tuvalet Kağıdı', 1, 1, 50, 10, 'Solo Tuvalet Kağıdı', 'https://cdn.dsmcdn.com/assets/dev/product/media/images/20200326/14/430345/13609709/1/1_org_zoom.jpg', '2020-03-31 15:32:30', '2020-04-04 11:21:20'),
 (17, 'Eyüp Sabri Tuncer Limon Kolonyası 400 Ml', 1, 1, 30, 12, 'Eyüp Sabri Tuncer Limon Kolonyası 400 Ml #EVDEKAL', 'https://cdn03.ciceksepeti.com/cicek/kcx59904516/XL/eyup-sabri-tuncer-cesme-limonu-kolonya-16-ml.jpeg', '2020-03-31 16:03:45', '2020-04-04 11:34:18'),
 (18, 'Canon EOS RP', 3, 1, 15000, 99, 'Canon Eos Rp + 24-105MM Is Usm', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSxDt9LB7W-AaW4vGNvqhvb0LkRLkSNGy2GvV2G_-EiswGvmirKsg&usqp=CAc', '2020-03-31 17:02:33', '2020-04-04 11:30:17'),
-(19, 'Columbia Wm1521 Cascade Peak™ II Jacket Mont', 7, 1, 1000, 5, 'Columbia Wm1521 Cascade Peak™ II Jacket Mont', 'https://statics.boyner.com.tr/mnresize/900/1254/productimages/5002479195_001_01.jpg', '2020-04-04 11:42:16', '2020-04-04 11:42:16');
+(19, 'Columbia Wm1521 Cascade Peak™ II Jacket Mont', 7, 1, 1000, 5, 'Columbia Wm1521 Cascade Peak™ II Jacket Mont', 'https://statics.boyner.com.tr/mnresize/900/1254/productimages/5002479195_001_01.jpg', '2020-04-04 11:42:16', '2020-04-04 11:42:16'),
+(20, 'Playstation 4', 9, 1, 5000, 10, 'Playstation 4', 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcStCqfEOh_ApEJWmwaD1dvojs7Fv_5DW7w_IrkSpnrun0OMEyQR3D8&usqp=CAc', '2020-04-05 08:16:16', '2020-04-05 08:16:42');
 
 -- --------------------------------------------------------
 
@@ -424,7 +415,7 @@ ALTER TABLE `adres`
 -- Tablo için AUTO_INCREMENT değeri `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `migrations`
@@ -454,19 +445,19 @@ ALTER TABLE `rolyetkileris`
 -- Tablo için AUTO_INCREMENT değeri `sepets`
 --
 ALTER TABLE `sepets`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `sepet_elemanlaris`
 --
 ALTER TABLE `sepet_elemanlaris`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `uruns`
 --
 ALTER TABLE `uruns`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `users`
